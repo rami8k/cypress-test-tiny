c486da515be58855e6bf78df6aae7a83b7bfdc82
Rami <rkhasawneh@bloombergindustry.com>
2022-02-25 23:25:55 -0500

    chore: fix the bug

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,4 +1,4 @@
 it('does not work', () => {
   cy.visit('https://news.bloomberglaw.com/interactive/banking-on-bankruptcy')
-  cy.get('banking-on-bankruptcy').shadow().find('.app').contains('Roy Strom')
+  cy.get('banking-on-bankruptcy').shadow().find('#app').contains('Roy Strom')
 })
